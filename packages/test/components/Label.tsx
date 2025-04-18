b9f4f997b94bbf4dcc2705fcd29d37dedb66540e
joaolessab <joaovitorlessa@gmail.com>
2025-04-18 18:23:31 -0300

    test

---
+++ packages/test/components/Label.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function Label() {
+  return (
+    <View className="flex-1 items-center justify-center bg-purple-700">
+      <Text className="text-center text-xl font-bold text-white">Label Component Imported!</Text>
+    </View>
+  );
+}
